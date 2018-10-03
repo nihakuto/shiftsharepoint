39574817ae58e9fcf6b71794ac14121a933e2501
Alexandre Carnet <carnetalexandre@gmail.com>
2018-10-03 01:55:54 +0100

    savedBrandID for returning visitor tracking

--- js/navbar-cookies.js
+++ js/navbar-cookies.js
@@ -1,4 +1,5 @@
 $(document).ready(function() {
+    var expirytime = 43200; // time in MINUTES
     if (lscache.get('popState') != 'shown') {
         $('#brand-bar-modal').delay(500).modal('show') // time in milliseconds
     }
@@ -9,15 +10,19 @@ $(document).ready(function() {
         });
     $('#NoAnswer').click(function(e) // You are clicking the close button
         {
+            var clickedID = this.id;
             $('#brand-bar-modal').modal('hide'); // Now the pop up is hiden.
-            lscache.set('popState', 'shown', 43200) // time in MINUTES
+            lscache.set('popState', 'shown', expirytime) // time in MINUTES
+            lscache.set('savedBrandID', clickedID, expirytime) // passed to Google Tag Manager
         });
     // $('#popup-container').click(function(e) {
     //     $('#brand-bar-modal').modal('hide');
     // });
 
     $('.brand-btn').click(function(){
+        var clickedID = this.id;
         $('#brand-bar-modal').modal('hide');
-        lscache.set('popState', 'shown', 43200) // time in MINUTES
+        lscache.set('popState', 'shown', expirytime) // time in MINUTES
+        lscache.set('savedBrandID', clickedID, expirytime) // passed to Google Tag Manager
     });
 });
